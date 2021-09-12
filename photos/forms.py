b9acdadd67de8888f7bd7b174aaeb0f04b2282c5
Dennis Mwaniki <damos5776@gmail.com>
2021-09-12 20:45:19 +0300

    form

--- photos/forms.py
+++ photos/forms.py
@@ -19,7 +19,7 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
-        widgets = {'bio': forms.Textarea(attrs={'rows':2, 'cols':10,}),}
+        idgets = {'bio': forms.Textarea(attrs={'rows':2, 'cols':10,}),}
         
         
 class RegisterForm(RegistrationForm):
