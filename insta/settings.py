35f000f19bdf872d98ded7216fe4dadc679201b3
Dennis Mwaniki <damos5776@gmail.com>
2021-09-12 21:11:03 +0300

    gitignore

--- insta/settings.py
+++ insta/settings.py
@@ -15,6 +15,8 @@ import os
 import dj_database_url
 from decouple import config,Csv
 
+
+
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
@@ -52,13 +54,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qx7sy2ost5mg15aq1)cslfkc)5imp@ei!pngv^9yc3%c^57i_4'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
