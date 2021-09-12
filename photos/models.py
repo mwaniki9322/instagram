52e85743f2bec57bfb909f79e4001bf361bab6e8
Dennis Mwaniki <damos5776@gmail.com>
2021-09-12 20:18:57 +0300

    save image

--- photos/models.py
+++ photos/models.py
@@ -43,7 +43,7 @@ class Image(models.Model):
     likes = models.ManyToManyField(User, related_name = 'likes', blank = True)
 
         
-    def save_image(s
+    def save_image(self):
         self.save()
     
     def delete_image(self):
